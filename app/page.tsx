1652a18d4fa0874c9b0ac8f2c74519586bdb9b64
Arun Patel <arunpatel209@gmail.com>
2025-02-23 17:51:34 +0530

    fixed build issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,6 @@ import Contact from "@/components/sections/Contact";
 import Home from "./(pages)/Home";
 import Experience from "@/components/sections/Experience";
 import Projects from "@/components/sections/Projects";
-import { StickyScrollRevealDemo } from "@/components/sections/StickyScrollDemo";
-import Footer from "@/components/Footer";
-import { sections } from "@/components/Header";
-
 
 export default function Page() {
   return (
--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FC, useRef } from "react";
+import React, { useRef } from "react";
 import ProjectCard from "@/components/ProjectCard";
 import projects from "@/app/data/projectData";
 
